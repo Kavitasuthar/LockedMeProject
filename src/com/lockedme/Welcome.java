7a56f1906d84814b0c82693da2d32183e6913e92
Kavita Suthar <kbaberwal@gmail.com>
2022-05-14 12:23:18 -0700

    Submtting another file which playing with git commands.

---
+++ src/com/lockedme/Welcome.java
@@ -0,0 +1,16 @@
+package com.lockedme;
+
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		
+		
+		BusinessValidation.welcomeScreen();
+		
+		BusinessValidation.inputFromUser();
+	
+	}
+}
+
+	
