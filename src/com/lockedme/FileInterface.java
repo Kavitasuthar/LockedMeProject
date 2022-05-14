03eb215955c89c10acd027cfcf591275b589beef
Kavita Suthar <kbaberwal@gmail.com>
2022-05-14 12:27:28 -0700

    testing a file change.

--- src/com/lockedme/FileInterface.java
+++ src/com/lockedme/FileInterface.java
@@ -3,7 +3,7 @@ package com.lockedme;
 
 public interface FileInterface {
 
-	public void addFile();
+	 public void addFile();
 	public void deleteFile();
 	public void searchFile();
 	public void showAllFiles();
